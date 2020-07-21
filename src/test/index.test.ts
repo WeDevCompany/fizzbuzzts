d0f02edadf6784a6bc502c45300c16c591512b37
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-07-21 12:51:23 +0200

    TDD-failing: Created test to cover the divisible by 3 and 5 at the same time

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -3,6 +3,7 @@ const fizzBuzzFunction = require('../index');
 describe('FizzBuzz should', () => {
   test('return empty string if is not divisible by 3 or 5', () => {
     expect(fizzBuzzFunction(1)).toBe('');
+    expect(fizzBuzzFunction(2)).toBe('');
   });
 
   test('return Fizz divisible by 3', () => {
@@ -11,5 +12,9 @@ describe('FizzBuzz should', () => {
 
   test('return Buzz divisible by 5', () => {
     expect(fizzBuzzFunction(5)).toBe('Buzz');
+  });
+
+  test('return Buzz divisible by 3 and 5', () => {
+    expect(fizzBuzzFunction(15)).toBe('FizzBuzz');
   });
 });
