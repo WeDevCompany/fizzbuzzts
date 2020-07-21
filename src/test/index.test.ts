d410bfa6becbe2e0585d84381c986794a798bafa
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-07-21 12:49:29 +0200

    TDD-passing: Created test to cover the empty string use case

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,6 +1,10 @@
 const fizzBuzzFunction = require('../index');
 
 describe('FizzBuzz should', () => {
+  test('return empty string if is not divisible by 3 or 5', () => {
+    expect(fizzBuzzFunction(1)).toBe('');
+  });
+
   test('return Fizz divisible by 3', () => {
     expect(fizzBuzzFunction(3)).toBe('Fizz');
   });
