37b2ebb2bdf6d886bc91a7a20868aae74278730a
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-07-21 12:55:28 +0200

    TDD-failing: Implementing the correct logic to solve the divisible by 3 and 5 use case (first refactor)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 function fizzBuzz(number: number): string {
+  if (number % 15 === 0) {
+    return 'FizzBuzz';
+  }
+
   if (number % 3 === 0) {
     return 'Fizz';
   }
 
   if (number % 5 === 0) {
     return 'Buzz';
-  }
-
-  if (number % 15 === 0) {
-    return 'FizzBuzz';
   }
 
   return '';
