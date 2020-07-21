98b53961dd6ba4263c87c6e0db692f0650c1fa9f
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-07-21 12:46:03 +0200

    TDD-failing: Modify test to fit the divisible by 5 use cases

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,7 +1,11 @@
 const fizzBuzzFunction = require('../index');
 
 describe('FizzBuzz should', () => {
-  test('return the if divisible by 3', () => {
+  test('return Fizz divisible by 3', () => {
     expect(fizzBuzzFunction(3)).toBe('Fizz');
+  });
+
+  test('return Buzz divisible by 5', () => {
+    expect(fizzBuzzFunction(5)).toBe('Buzz');
   });
 });
