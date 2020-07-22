8be64521d90c373b2ac2b7272fb585362f463bff
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2020-07-22 15:52:43 +0200

    Adding test to return the same number

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 function fizzBuzz(number: number): string {
-  if (number % 15 === 0) {
-    return 'FizzBuzz';
-  }
+	if (number % 15 === 0) {
+		return 'FizzBuzz';
+	}
 
-  if (number % 3 === 0) {
-    return 'Fizz';
-  }
+	if (number % 3 === 0) {
+		return 'Fizz';
+	}
 
-  if (number % 5 === 0) {
-    return 'Buzz';
-  }
+	if (number % 5 === 0) {
+		return 'Buzz';
+	}
 
-  return '';
+	return number.toString();
 }
 
 module.exports = fizzBuzz;
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,20 +1,20 @@
 const fizzBuzzFunction = require('../index');
 
 describe('FizzBuzz should', () => {
-  test('return empty string if is not divisible by 3 or 5', () => {
-    expect(fizzBuzzFunction(1)).toBe('');
-    expect(fizzBuzzFunction(2)).toBe('');
-  });
+	test('return empty string if is not divisible by 3 or 5', () => {
+		expect(fizzBuzzFunction(1)).toBe('1');
+		expect(fizzBuzzFunction(2)).toBe('2');
+	});
 
-  test('return Fizz divisible by 3', () => {
-    expect(fizzBuzzFunction(3)).toBe('Fizz');
-  });
+	test('return Fizz divisible by 3', () => {
+		expect(fizzBuzzFunction(3)).toBe('Fizz');
+	});
 
-  test('return Buzz divisible by 5', () => {
-    expect(fizzBuzzFunction(5)).toBe('Buzz');
-  });
+	test('return Buzz divisible by 5', () => {
+		expect(fizzBuzzFunction(5)).toBe('Buzz');
+	});
 
-  test('return Buzz divisible by 3 and 5', () => {
-    expect(fizzBuzzFunction(15)).toBe('FizzBuzz');
-  });
+	test('return Buzz divisible by 3 and 5', () => {
+		expect(fizzBuzzFunction(15)).toBe('FizzBuzz');
+	});
 });
